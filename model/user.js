c54092501aecfd80bf3c157dbf3142ffd078df5c
tejpalsinhrana <tejpalsinh.karavyasolutions@gmail.com>
2022-04-04 12:12:26 +0530

    SET env file

--- model/user.js
+++ model/user.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     username: { type: String, unique: true },
-    firstName:String,
-    lastName:String,
-    email:String,
-    password:String,
-    phone:Number,
-    gender:String,
-    role:String,
+    firstName: { type: String, default: null },
+    lastName: { type: String, default: null },
+    email: { type: String, unique: true },
+    password: { type: String },
+    phone: { type: Number },
+    gender: { type: String },
+    role: { type: String },
 })
 
 module.exports = mongoose.model('User', userSchema);
